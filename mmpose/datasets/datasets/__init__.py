670ed9672cefa030f23724c3dd26813c599ed729
AshwiniR97 <ashwini.rajasekhar@gmail.com>
2023-01-26 23:34:26 +0530

    Updated init.py to suit body detection

--- mmpose/datasets/datasets/__init__.py
+++ mmpose/datasets/datasets/__init__.py
@@ -1,6 +1,8 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 from ...deprecated import (TopDownFreiHandDataset, TopDownOneHand10KDataset,
                            TopDownPanopticDataset)
+
+# /mmpose/mmpose/datasets/datasets/init.py
 from .animal import (AnimalATRWDataset, AnimalFlyDataset, AnimalHorse10Dataset,
                      AnimalLocustDataset, AnimalMacaqueDataset,
                      AnimalPoseDataset, AnimalZebraDataset)
@@ -27,6 +29,27 @@ from .top_down import (TopDownAicDataset, TopDownCocoDataset,
                        TopDownOCHumanDataset, TopDownPoseTrack18Dataset,
                        TopDownPoseTrack18VideoDataset)
 
+#__all__ = [
+#     'TopDownCocoDataset', 'BottomUpCocoDataset', 'BottomUpMhpDataset',
+#     'BottomUpAicDataset', 'BottomUpCocoWholeBodyDataset', 'TopDownMpiiDataset',
+#     'TopDownMpiiTrbDataset', 'OneHand10KDataset', 'PanopticDataset',
+#     'HandCocoWholeBodyDataset', 'FreiHandDataset', 'InterHand2DDataset',
+#     'InterHand3DDataset', 'TopDownOCHumanDataset', 'TopDownAicDataset',
+#     'TopDownCocoWholeBodyDataset', 'MeshH36MDataset', 'MeshMixDataset',
+#     'MoshDataset', 'MeshAdversarialDataset', 'TopDownCrowdPoseDataset',
+#     'BottomUpCrowdPoseDataset', 'TopDownFreiHandDataset',
+#     'TopDownOneHand10KDataset', 'TopDownPanopticDataset',
+#     'TopDownPoseTrack18Dataset', 'TopDownJhmdbDataset', 'TopDownMhpDataset',
+#     'DeepFashionDataset', 'Face300WDataset', 'FaceAFLWDataset',
+#     'FaceWFLWDataset', 'FaceCOFWDataset', 'FaceCocoWholeBodyDataset',
+#     'Body3DH36MDataset', 'AnimalHorse10Dataset', 'AnimalMacaqueDataset',
+#     'AnimalFlyDataset', 'AnimalLocustDataset', 'AnimalZebraDataset',
+#     'AnimalATRWDataset', 'AnimalPoseDataset', 'TopDownH36MDataset',
+#     'TopDownHalpeDataset', 'TopDownPoseTrack18VideoDataset',
+#     'Body3DMviewDirectPanopticDataset', 'Body3DMviewDirectShelfDataset',
+#     'Body3DMviewDirectCampusDataset', 'NVGestureDataset'
+# ]
+
 __all__ = [
     'TopDownCocoDataset', 'BottomUpCocoDataset', 'BottomUpMhpDataset',
     'BottomUpAicDataset', 'BottomUpCocoWholeBodyDataset', 'TopDownMpiiDataset',
@@ -35,9 +58,7 @@ __all__ = [
     'InterHand3DDataset', 'TopDownOCHumanDataset', 'TopDownAicDataset',
     'TopDownCocoWholeBodyDataset', 'MeshH36MDataset', 'MeshMixDataset',
     'MoshDataset', 'MeshAdversarialDataset', 'TopDownCrowdPoseDataset',
-    'BottomUpCrowdPoseDataset', 'TopDownFreiHandDataset',
-    'TopDownOneHand10KDataset', 'TopDownPanopticDataset',
-    'TopDownPoseTrack18Dataset', 'TopDownJhmdbDataset', 'TopDownMhpDataset',
+    'BottomUpCrowdPoseDataset', 'TopDownPoseTrack18Dataset', 'TopDownJhmdbDataset', 'TopDownMhpDataset',
     'DeepFashionDataset', 'Face300WDataset', 'FaceAFLWDataset',
     'FaceWFLWDataset', 'FaceCOFWDataset', 'FaceCocoWholeBodyDataset',
     'Body3DH36MDataset', 'AnimalHorse10Dataset', 'AnimalMacaqueDataset',
